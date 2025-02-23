4950665931d8e203f6fc83cecd022b88c74049d8
Mehdi Armachi <armachi.mehdi@gmail.com>
2025-02-23 15:46:07 +0000

    init

---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Update and upgrade system
+echo "Updating system..."
+sudo apt update && sudo apt upgrade -y
+
+# Install essential tools
+echo "Installing essential tools..."
+sudo apt install -y git curl wget unzip zip tmux htop tree fzf ripgrep bat exa neovim zsh
+
+# Install Oh My Zsh if not installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    chsh -s $(which zsh)
+fi
+
+# Install Powerlevel10k theme
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
+    echo "Installing Powerlevel10k..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    sed -i 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+fi
+
+# Install Zsh plugins
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+    echo "Installing Zsh plugins..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/' ~/.zshrc
+fi
+
+# Set up .bashrc and .zshrc aliases
+echo "Configuring aliases..."
+cat <<EOF >> ~/.bashrc
+# Custom Aliases
+alias cl="clear"
+EOF
+
+# Apply changes
+echo "Applying changes..."
+source ~/.bashrc
+source ~/.zshrc
+
+echo "Setup complete! Restart your terminal or run 'exec zsh' to switch."
